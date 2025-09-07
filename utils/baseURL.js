a5b9c14467c218611bd121ccfa623756de8fd946
victorrmark <innovimark@gmail.com>
2025-09-07 10:53:04 +0100

    env

--- utils/baseURL.js
+++ utils/baseURL.js
@@ -1,11 +1,15 @@
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") {
-    return "";
+    return ""; // browser
+  }
+
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL; // your own env var
   }
 
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  return "http://localhost:3000";
-}
+  return "http://localhost:3000"; // local
+};
